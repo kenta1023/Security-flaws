162d41dc99fadfd1b1ac4c61e43c8e926d2d29ce
Kenta <c.kenta2018@gmail.com>
2023-06-05 01:46:33 +0900

    完成（SameSite属性をNoneにapacheで設定）

--- csrf/php/index.php
+++ csrf/php/index.php
@@ -21,10 +21,14 @@ if (isset($_SESSION['id'])) {
 <body>
     <section class="main">
         <h1>ようこそ---<strong><?php echo $name; ?></strong>---</h1>
-        <h2>ログイン完了です</h2>
+        <h2>ログイン済みです。</h2>
         <p>
             CSRFでは他のページを訪れた際に仕組まれているPOSTにより、勝手にCSRFの脆弱性を含むこのページ上の操作を行われてしまう。
         </p>
+        <p>
+            このページに対してCSRFを利用しパスワードを勝手に変更する攻撃を仕掛けるコードを含むページ。<br>
+            <a href="http://localhost:8011/">http://localhost:8011/</a>
+        </P>
         <button type="button" onclick="location.href='logout.php'">ログアウト</button>
         <button type="button" onclick="location.href='change-password.php'">パスワード変更</button>
     </section>
